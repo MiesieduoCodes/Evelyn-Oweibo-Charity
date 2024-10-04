5ce532e45d00e4f07b8b53cc4f41cf22ab8cfeb3
MiesieduoCodes <miesieduoveria@gmail.com>
2024-10-04 23:57:49 +0100

    josha ooo

--- src/Components/MiniSection.jsx
+++ src/Components/MiniSection.jsx
@@ -1,36 +1,41 @@
-
 const MiniSection = () => {
   return (
-    <div className="flex justify-between p-10">
-        <div className="text-center">
-            <div></div>
-
-            <div>
-            <h1><b>HeaderText</b></h1>
-            <p>Lorem ipsum, dolor sit amet consec<br /> 
-            tetur adipisicing elit. Rem sequi natus<br />  similique pariatur molestiae quibusdam tempora.</p>
-            </div>
+    <div className="flex flex-col md:flex-row justify-between p-5 md:p-10 space-y-6 md:space-y-0 md:space-x-6">
+      <div className="text-center flex-1">
+        <div></div>
+        <div>
+          <h1 className="text-xl font-bold">Header Text</h1>
+          <p className="text-gray-700">
+            Lorem ipsum, dolor sit amet consec<br />
+            tetur adipisicing elit. Rem sequi natus<br />
+            similique pariatur molestiae quibusdam tempora.
+          </p>
         </div>
-        <div className="text-center">
-            <div></div>
-            
-            <div>
-            <h1><b>HeaderText</b></h1>
-            <p>Lorem ipsum, dolor sit amet consec<br /> 
-            tetur adipisicing elit. Rem sequi natus<br />  similique pariatur molestiae quibusdam tempora.</p>
-            </div>
+      </div>
+      <div className="text-center flex-1">
+        <div></div>
+        <div>
+          <h1 className="text-xl font-bold">Header Text</h1>
+          <p className="text-gray-700">
+            Lorem ipsum, dolor sit amet consec<br />
+            tetur adipisicing elit. Rem sequi natus<br />
+            similique pariatur molestiae quibusdam tempora.
+          </p>
         </div>
-        <div className="text-center">
-            <div></div>
-            
-            <div>
-            <h1><b>HeaderText</b></h1>
-            <p>Lorem ipsum, dolor sit amet consec<br /> 
-            tetur adipisicing elit. Rem sequi natus<br />  similique pariatur molestiae quibusdam tempora.</p>
-            </div>
+      </div>
+      <div className="text-center flex-1">
+        <div></div>
+        <div>
+          <h1 className="text-xl font-bold">Header Text</h1>
+          <p className="text-gray-700">
+            Lorem ipsum, dolor sit amet consec<br />
+            tetur adipisicing elit. Rem sequi natus<br />
+            similique pariatur molestiae quibusdam tempora.
+          </p>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default MiniSection
+export default MiniSection;
